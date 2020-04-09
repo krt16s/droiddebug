c725d20a7e70b0164cb88448cb3d32ebd410973f
Boris Kraut <krt@nurfuerspam.de>
2020-04-09 18:20:56 +0200

    Update example

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,23 +15,15 @@ public class App {
     public static void main(String[] args) {
         try {
             long filesize = new File(args[1]).length();
-            for (long sl = 2 * filesize; sl > filesize - 1000L; sl = sl - 1000L) {
-                doDroidIdent(args[0], args[1], sl);
-                System.out.println("---");
-            }
-            for (long sl = filesize; sl >= -1L; sl = sl - 1000L) {
-                doDroidIdent(args[0], args[1], sl);
-                System.out.println("---");
-            }
+            doDroidIdent(args[0], args[1], filesize);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
             System.exit(-1);
         }
     }
 
-    public static void doDroidIdent(String sigfile, String idfile, long sizelimit) throws Exception {
+    public static void doDroidIdent(String sigfile, String idfile, long size) throws Exception {
         File file = new File(idfile);
-        System.out.println("MEMINFO: " + Runtime.getRuntime().maxMemory() + " max, " + Runtime.getRuntime().freeMemory() + " free, " + Runtime.getRuntime().totalMemory() + " total, " + sizelimit + " sizelimit, " + file.length() + " filesize");
 
         BinarySignatureIdentifier mysig = new BinarySignatureIdentifier();
         mysig.setSignatureFile(sigfile);
@@ -39,8 +31,7 @@ public class App {
 
         IdentificationResultCollection results = null;
 
-        //sizelimit = file.length();
-        RequestMetaData metadata = new RequestMetaData(sizelimit, file.lastModified(), file.getName());
+        RequestMetaData metadata = new RequestMetaData(size, file.lastModified(), file.getName());
         RequestIdentifier identifier = new RequestIdentifier(file.toURI());
         FileSystemIdentificationRequest request = null;
 
@@ -68,7 +59,7 @@ public class App {
             System.out.println("RESULT: droid-mimetype:" + res.getMimeType() + ", droid-typename:" + res.getName() + ", droid-puid:" + res.getPuid() + ", droid-x-version:" + res.getVersion() + ", droid-x-extid:" + res.getExtId());
         }
 
-        System.out.println("MEMINFO: " + Runtime.getRuntime().maxMemory() + " max, " + Runtime.getRuntime().freeMemory() + " free, " + Runtime.getRuntime().totalMemory() + " total, " + sizelimit + " sizelimit, " + file.length() + " filesize");
+        System.out.println("MEMINFO: " + Runtime.getRuntime().maxMemory() + " max, " + Runtime.getRuntime().freeMemory() + " free, " + Runtime.getRuntime().totalMemory() + " total, " + size + " size, " + file.length() + " filesize");
     }
 
 }
